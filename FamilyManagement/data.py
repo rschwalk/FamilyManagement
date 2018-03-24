9e589ab573206a32b8411c1e02376249e25757e6
Richard Schwalk <richard.schwalk@outlook.com>
2018-03-24 15:35:17 +0100

    data added

---
+++ FamilyManagement/data.py
@@ -0,0 +1,26 @@
+def items():
+    items = [
+        {
+            'id': 1,
+            'title': 'Task One',
+            'notes': 'Notes to task 1',
+            'creator': 'Richard',
+            'date': '24.03.2018'
+        },
+        {
+            'id': 2,
+            'title': 'Task Two',
+            'notes': 'Notes to task 2',
+            'creator': 'Vanessa',
+            'date': '24.03.2018'
+        },
+        {
+            'id': 3,
+            'title': 'Task Three',
+            'notes': 'Notes to task 3',
+            'creator': 'Vanessa',
+            'date': '24.03.2018'
+        }
+    ]
+
+    return items
