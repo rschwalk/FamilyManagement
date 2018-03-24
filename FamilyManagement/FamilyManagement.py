3a83bdf5ab5f967ee51d7ca47268cb4d991d4306
Richard Schwalk <richard.schwalk@outlook.com>
2018-03-24 16:27:21 +0100

    item.html added

--- FamilyManagement/FamilyManagement.py
+++ FamilyManagement/FamilyManagement.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
-import sys
+from data import items
 
 
 app = Flask(__name__)
 
+
+data_items = items()
 
 @app.route('/')
 def home():
@@ -12,6 +14,14 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/items')
+def items():
+    return render_template('items.html', items=data_items)
+
+@app.route('/item/<string:id>/')
+def item(id):
+    return render_template('item.html', id=id)
 
 if __name__ == "__main__":
     app.run(debug=True)
