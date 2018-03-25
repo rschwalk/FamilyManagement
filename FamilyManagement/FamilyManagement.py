4057e649c4b4392cf0d5469f0c658ec497622e76
Richard Schwalk <richard.schwalk@outlook.com>
2018-03-25 16:07:03 +0200

    Submit data via register form

--- FamilyManagement/FamilyManagement.py
+++ FamilyManagement/FamilyManagement.py
@@ -3,16 +3,17 @@ Family Management application - rschwalk 2018
 The main file for the Flask application.
 """
 
-from flask import Flask, render_template, flash, redirect, url_for, session, logging
+from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
 from wtforms import Form, TextAreaField, StringField, PasswordField, validators
 from passlib.hash import sha256_crypt
-from data import items
+from data import items, UserData
 
 
 app = Flask(__name__)
 
 
 DATA_ITEMS = items()
+# DATA_USERS = UserData().read_all()
 
 @app.route('/')
 def home():
@@ -32,7 +33,8 @@ def items_page():
 @app.route('/item/<string:id>/')
 def item(id):
     """ Item page """
-    return render_template('item.html', id=id)
+    user_data = UserData().read_all()
+    return render_template('item.html', item=user_data)
 
 
 class RegisterForm(Form):
@@ -46,6 +48,21 @@ class RegisterForm(Form):
     confirm = PasswordField('Confirm Password')
 
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(form.password.data)
+
+        data = UserData()
+        data.add_user(name, username, password)
+
+        # flash('You are now registered, and can log in', 'success')
+        redirect(url_for('index'))
+    
+    return render_template('register.html', form=form)
 
 
 if __name__ == "__main__":
